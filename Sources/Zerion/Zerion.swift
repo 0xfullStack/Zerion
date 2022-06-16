7fd0653195c3e807a43540bbde79463f6268053f
Lin <linshizai.me@gmail.com>
2022-06-16 20:45:13 +0800

    add reconnect listenning

--- Sources/Zerion/Zerion.swift
+++ Sources/Zerion/Zerion.swift
@@ -8,6 +8,8 @@ import SocketIO
 import Foundation
 import RxSwift
 import ReactiveX
+import Reachability
+import UIKit
 
 public final class Zerion {
     
@@ -19,6 +21,12 @@ public final class Zerion {
     private let url = "wss://api-v4.zerion.io"
     private let apiKey = "Demo.ukEVQp6L5vfgxcz4sBke7XvS873GMYHy"
     private let bag = DisposeBag()
+    private lazy var reachability: Reachability? = {
+        Reachability()
+    }()
+    public lazy var reachabilitySignal: Observable<Bool> = {
+        reachability?.rx.isReachable ?? .never()
+    }()
     private lazy var mannager: SocketManager = {
         return SocketManager(socketURL: URL(string: url)!, config: [
             .log(false), .forceWebsockets(true),
@@ -41,7 +49,16 @@ public final class Zerion {
     }
     
     private func subscribeReachability() {
-        
+        let reachable = reachabilitySignal.filter { $0 }.startWith(true).map { _ in () }
+        let enterForeground = UIApplication.rx.willEnterForeground.asObservable().startWith(())
+        Observable
+            .combineLatest(connectStatus, reachable, enterForeground)
+            .subscribe(onNext: { [weak self] (connectStatus, reachable, enterForeground) in
+                guard let self = self else { return }
+                guard !connectStatus else { return }
+                self.proxy.connectIfNeed()
+            })
+            .disposed(by: bag)
     }
     
     public func on(_ event: Event) -> Observable<[Any]> {
